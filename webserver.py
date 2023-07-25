bda89079c4b3561e56896d2113407a55b3745581
vigneshraaj00 <vigneshpolur6@gmail.com>
2023-07-25 05:44:41 +0000

    firstcommit

--- webserver.py
+++ webserver.py
@@ -1,33 +1,3 @@
-# Developing a Simple Webserver
-
-# AIM:
-
-To develop a simple webserver to serve html programming pages.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-HTML content creation is done
-
-### Step 2:
-
-Design of webserver workflow
-
-### Step 3:
-
-Implementation using Python code
-
-### Step 4:
-
-Serving the HTML pages.
-
-### Step 5:
-
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer,BaseHTTPRequestHandler
 
 content='''
@@ -59,15 +29,3 @@ print("This is my webserver")
 server_address =('',8000)
 httpd = HTTPServer(server_address,MyServer)
 httpd.serve_forever()
-```
-
-## OUTPUT:
-
-### client output:
-![clientoutput](clientoutput.jpeg)
-
-### server output:
-![serveroutput](serveroutput.jpeg)
-
-## RESULT:
-The program is executed succesfully.
